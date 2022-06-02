5ba64e79e505066ed5b1a28a6c6c7fed0bd9a721
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-06-02 22:57:52 +0200

    Adaptation taille fenetre

--- src/laby/MainLaby.java
+++ src/laby/MainLaby.java
@@ -2,18 +2,20 @@ package laby;
 
 import moteurJeu.MoteurJeu;
 
+import static laby.Constantes.TAILLE;
+
 public class MainLaby {
 	public static void main(String[] args) throws Exception {
 
-		int width = 800;
-		int height = 600;
-		int pFPS = 10;
 
 		// creation des objets
 //		LabyJeu jeuLaby = new LabyJeu("source/laby0.txt");
-		LabyJeu jeuLaby = new LabyJeu("source/V2/labyMonstre.txt");
-
+		LabyJeu jeuLaby = new LabyJeu("source/V3/labyAmulette.txt");
 		LabyDessin dessinLaby = new LabyDessin();
+
+		int width = jeuLaby.getLength() * TAILLE;
+		int height = jeuLaby.getLengthY() * TAILLE + 15;
+		int pFPS = 5;
 
 		// parametrage du moteur de jeu
 		MoteurJeu.setTaille(width, height);
