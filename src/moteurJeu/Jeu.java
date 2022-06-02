cd37bb3a5ca4bed9b37512cc907a789f32a46852
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-06-02 15:44:50 +0200

    Initial Commit

---
+++ src/moteurJeu/Jeu.java
@@ -0,0 +1,20 @@
+package moteurJeu;
+
+
+/**
+ * modele de jeu
+ */
+public interface Jeu {
+
+    /**
+     * methode mise a jour du jeu
+     * @param secondes temps ecoule depuis la derniere mise a jour
+     * @param clavier objet contenant l'état du clavier'
+     */
+    void update(double secondes, Clavier clavier);
+
+    /**
+     * initialisation du jeu
+     */
+    void init();
+}
