0d74b2c38fe3832725ae1ae4bfe8c5f541b89ee3
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-06-02 20:40:49 +0200

    Ajout monstre

---
+++ src/laby/ennemis/Monstre.java
@@ -0,0 +1,14 @@
+package laby.ennemis;
+
+import laby.Entite;
+import laby.Labyrinthe;
+
+import java.util.Random;
+
+public class Monstre extends Entite {
+
+	public Monstre(int x, int y) {
+		super(x, y);
+	}
+
+}
