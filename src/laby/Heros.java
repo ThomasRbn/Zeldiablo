89fabb89cee743750d715ae1732387d73a850290
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2022-06-02 20:41:14 +0200

    Modification emplacement méthode

--- src/laby/Heros.java
+++ src/laby/Heros.java
@@ -12,14 +12,4 @@ public class Heros extends Entite{
         super(dx, dy);
     }
 
-    /**
-     * permet de savoir si le personnage est en x,y
-     *
-     * @param dx position testee
-     * @param dy position testee
-     * @return true si le personnage est bien en (dx,dy)
-     */
-    public boolean etrePresent(int dx, int dy) {
-        return (this.getX() == dx && this.getY() == dy);
-    }
 }
